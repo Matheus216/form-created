9833aa810c590948aa9c9877b767943e342c1e6f
Daniel Richard <daniel.richard@starcorp.com.br>
2020-11-02 00:16:08 -0300

    add

--- Forms/ConsultaEmpresa/ConsultaEmpresa.js
+++ Forms/ConsultaEmpresa/ConsultaEmpresa.js
@@ -7,12 +7,37 @@ window.companyGet.UI = {
 
 window.companyGet.functions = {
     initializer: () => {
-    }
+    },
+
+    getCompanyName: () => {
+        let companyName = $('#companynName').val();
+
+        $.ajax({
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: `${config.url}api/Establishment/GetEstablishmentByCompanyName?companyName=${companyName}`,
+            crossDomain:true,
+            success: function (response) {
+                alert(response);
+            },
+            error: function (err) {
+                window.functions.messageReturn('modal-alert', 'Erro ao buscar empresa !', false);
+            }
+        });
+    },
 }
 
 window.companyGet.Events = {
     initializer: () => {
-    }
+        $('#companynName').change(() => window.companyGet.Events.onChangeCompanyName());
+    },
+
+    onChangeCompanyName: () => {
+        window.companyGet.functions.getCompanyName();
+    },
+
+
 }
 
 window.companyGet.UI.initializer();
